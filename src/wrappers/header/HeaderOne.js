854f0145e44103ae2c8bf9b3748ad8d2bade8c73
Muhammad Abdullah Qureshi <maqureshi94@hotmail.com>
2022-12-16 16:14:28 +0500

    home page done

--- src/wrappers/header/HeaderOne.js
+++ src/wrappers/header/HeaderOne.js
@@ -23,40 +23,27 @@ const HeaderOne = (props) => {
 
   return (
     <header className={`header-area clearfix  ${"headerPositionClass"}`}>
-      <div className={`${"headerPaddingClass"} header-top-area`}></div>
+      {/* <div className={`${"headerPaddingClass"} header-top-area`}></div> */}
 
-      <div
-        className={` ${"headerPaddingClass"} sticky-bar header-res-padding clearfix ${scroll > headerTop ? "stick" : ""
-          }`}
-      >
-        <div 
-
-        className={"container webView"}>
+      <div className={` ${"headerPaddingClass"} sticky-bar header-res-padding clearfix ${scroll > headerTop ? "stick" : "" }`} >
+        <div className={"container webView"}>
           <div className="row">
             <div className="d-flex col-xl-6 col-lg-6 col-md-6 col-6">
               <Logo imageUrl="/assets/img/logo/logo.png" logoClass="logo" />
             </div>
-
             <div className="col-xl-6 col-lg-6 col-md-6 col-6 ">
               <SocialMediaGroup />
             </div>
-
-
           </div>
-
           <div className="row">
-
             <div className="col-xl-9 col-lg-9 d-none d-lg-block">
               <NavMenu />
             </div>
             <div className="col-xl-3 col-lg-3 col-md-6 col-7">
               <IconGroup />
             </div>
-
-
           </div>
         </div>
-
         <MobileMenu />
       </div>
     </header>
