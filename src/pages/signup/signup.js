17f15ae05f3e54661ee961eed4c93cf3fa5fc5d8
Muhammad Muzamil <121708782+Mohammad-Muzamil@users.noreply.github.com>
2023-08-15 18:29:48 +0500

    sign up basic structure started

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -9,6 +9,50 @@ import btnArrowGt from "../../assets/img/hero-btn-arrow-gt.svg";
 import login from "../../assets/img/login.png"
 import googleicon from "../../assets/img/googleicon.png"
 
+import '../../assets/css/blurredbackground.css';
+
+function BlurredBackgroundModal() {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
+    const handleOpenModal = () => {
+        setShowModal(true);
+    };
+
+    return (
+        <div className="modal-demo">
+            <button className="open-modal-btn" onClick={handleOpenModal}>
+                Open Modal
+            </button>
+
+            {showModal && (
+                <div className="modal-overlay">
+                    <div className="modal-container">
+                        <div className="modal-content">
+                            <button className="close-modal-btn" onClick={handleCloseModal}>
+                                &times;
+                            </button>
+                            <h2>Modal Title</h2>
+                            <p>This is the modal content.</p>
+                            <div className="modal-buttons">
+                                <button className="close-btn" onClick={handleCloseModal}>
+                                    Close
+                                </button>
+                                <button className="save-btn">Save Changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+
+
 
 
 
@@ -22,7 +66,7 @@ const SignUp = (props) => {
         headerPaddingClass="header-padding-2"
       >
         <div className="pt-100 pb-100">
-        
+        <BlurredBackgroundModal></BlurredBackgroundModal>
         </div>
       </LayoutOne>
     </Fragment>
