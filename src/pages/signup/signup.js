fcfd6711a50a06f648c311a0f771dfc0889fb338
Muhammad Muzamil <121708782+Mohammad-Muzamil@users.noreply.github.com>
2023-08-16 16:29:45 +0500

    sign up image done

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -11,6 +11,9 @@ import googleicon from "../../assets/img/googleicon.png"
 
 import '../../assets/css/blurredbackground.css';
 
+import prof_icon from "../../assets/img/prof-icon.png"
+import camera from "../../assets/img/camera.png"
+
 
 
 
@@ -19,9 +22,15 @@ function BlurredBackgroundModal() {
     const [showModal, setShowModal] = useState(false);
     const [inputValue, setInputValue] = useState('');
     const [selectedImage, setSelectedImage] = useState(null);
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        setIsMobile(/Mobi|Android/i.test(navigator.userAgent));
+    }, []);
 
     const handleCloseModal = () => {
         setShowModal(false);
+        console.log("imagepath", selectedImage)
     };
 
     const handleOpenModal = () => {
@@ -31,6 +40,7 @@ function BlurredBackgroundModal() {
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
     };
+   
 
     const handleImageSelect = (e) => {
         const file = e.target.files[0];
@@ -40,8 +50,10 @@ function BlurredBackgroundModal() {
     };
 
     const handleSaveChanges = () => {
-        console.log('Saving changes:', inputValue);
         handleCloseModal();
+    };
+    const openFileInput = () => {
+      document.getElementById("getimg").click();
     };
 
     return (
@@ -57,22 +69,35 @@ function BlurredBackgroundModal() {
                             <button className="close-modal-btn" onClick={handleCloseModal}>
                                 &times;
                             </button>
-                            <h2>Select Image</h2>
+  
+                            <p className="text-center headingmain" style={{fontFamily: "Ethnocentric"}}>Profile Image</p>
+                            {selectedImage &&<p className="text-center mt-1 alertmsg">Uploaded Successfully</p>}
                             <div className="image-container">
-                                {selectedImage && <img src={selectedImage} alt="Selected" />}
+                                {selectedImage ? <img src={selectedImage} alt="Selected" />:<img src={prof_icon}/>}
                             </div>
                             <input
                                 type="file"
                                 accept="image/*"
                                 onChange={handleImageSelect}
+                                style={{display:"none"}}
+                                capture="camera"
+                            id="getimg"
                             />
                             <div className="modal-buttons">
-                                <button className="close-btn" onClick={handleCloseModal}>
+                                {!isMobile &&<button className="close-btn"  style={{fontFamily: "Ethnocentric"}}onClick={handleCloseModal}>
                                     Close
+                                </button>}
+                                {isMobile && 
+                                 <button className="" style={{marginRight:"10px"}}  onClick={openFileInput}>
+                                     <img src={camera} style={{width:"30px", height:"30px"}}/>
+                                </button>}
+                                <button className="save-btn" onClick={openFileInput}>
+                                     Upload
                                 </button>
-                                <button className="save-btn" onClick={handleSaveChanges}>
+                                
+                                {/* <button className="save-btn" onClick={handleSaveChanges}>
                                     Save Changes
-                                </button>
+                                </button> */}
                             </div>
                         </div>
                     </div>
@@ -82,6 +107,47 @@ function BlurredBackgroundModal() {
     );
 }
 
+
+
+const MultiPartForm = () => {
+    const [currentPart, setCurrentPart] = useState(1);
+
+    const handleNext = () => {
+        setCurrentPart(currentPart + 1);
+    };
+
+    const handlePrevious = () => {
+        setCurrentPart(currentPart - 1);
+    };
+
+    return (
+        <div className="form-container">
+            <div className="form">
+                <div className="steps">
+                    <div className={`step ${currentPart === 1 ? 'active' : ''}`}>Step 1</div>
+                    <div className={`step ${currentPart === 2 ? 'active' : ''}`}>Step 2</div>
+                    <div className={`step ${currentPart === 3 ? 'active' : ''}`}>Step 3</div>
+                </div>
+                <div className={`form-part ${currentPart === 1 ? 'active' : ''}`}>
+                    <h2>Part 1</h2>
+                    {/* Your form fields for Part 1 */}
+                    <button onClick={handleNext}>Next</button>
+                </div>
+                <div className={`form-part ${currentPart === 2 ? 'active' : ''}`}>
+                    <h2>Part 2</h2>
+                    {/* Your form fields for Part 2 */}
+                    <button onClick={handlePrevious}>Previous</button>
+                    <button onClick={handleNext}>Next</button>
+                </div>
+                <div className={`form-part ${currentPart === 3 ? 'active' : ''}`}>
+                    <h2>Part 3</h2>
+                    {/* Your form fields for Part 3 */}
+                    <button onClick={handlePrevious}>Previous</button>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 
 
@@ -96,8 +162,12 @@ const SignUp = (props) => {
         headerContainerClass="container-fluid"
         headerPaddingClass="header-padding-2"
       >
-        <div className="pt-100 pb-100">
-        <BlurredBackgroundModal></BlurredBackgroundModal>
+        <div className="BackgroundPicture pt-100 pb-100 d-flex justify-content-center">
+            <div className="col-lg-7  bg-white col-10" style={{height:"400px"}} >
+                <div>
+
+                </div>
+            </div>
         </div>
       </LayoutOne>
     </Fragment>
