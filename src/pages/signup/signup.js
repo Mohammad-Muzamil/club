03f27cd8fc01f422ddadf10ea5ea916b8329a077
Muhammad Muzamil <121708782+Mohammad-Muzamil@users.noreply.github.com>
2023-08-21 17:42:19 +0500

    sign up done

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -8,11 +8,15 @@ import btnArrowLt from "../../assets/img/hero-btn-arrow-lt.svg";
 import btnArrowGt from "../../assets/img/hero-btn-arrow-gt.svg";
 import login from "../../assets/img/login.png"
 import googleicon from "../../assets/img/googleicon.png"
+import cloud from "../../assets/img/cloud.png"
 
 import '../../assets/css/blurredbackground.css';
+import '../../assets/css/signup.css';
 
 import prof_icon from "../../assets/img/prof-icon.png"
 import camera from "../../assets/img/camera.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -57,10 +61,12 @@ function BlurredBackgroundModal() {
     };
 
     return (
-        <div className="modal-demo">
-            <button className="open-modal-btn" onClick={handleOpenModal}>
-                Open Modal
-            </button>
+        <div className="">
+           
+            <div className="col-lg-3 col-12 d-flex  flex-column justify-content-center"  onClick={handleOpenModal} style={{border:"2px dashed gray", alignItems:"center", height:"150px"}}>
+                <img src={cloud} style={{height:"50px", width:"50px"}} />
+                <p style={{fontFamily: "mont", color:"#87CEEB"}}>click to upload</p>
+            </div>
 
             {showModal && (
                 <div className="modal-overlay">
@@ -119,6 +125,7 @@ const MultiPartForm = () => {
     const handlePrevious = () => {
         setCurrentPart(currentPart - 1);
     };
+   
 
     return (
         <div className="form-container">
@@ -156,7 +163,12 @@ const MultiPartForm = () => {
 
 
 const SignUp = (props) => {
-    const [currentPart, setCurrentPart] = useState(2);
+    const [currentPart, setCurrentPart] = useState(1);
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
 
     const handleNext = () => {
         setCurrentPart(currentPart + 1);
@@ -165,29 +177,210 @@ const SignUp = (props) => {
     const handlePrevious = () => {
         setCurrentPart(currentPart - 1);
     };
+    const [Mobile, setMobile] = useState(false);
+
+   
+    useEffect(() => {
+        const handleResize = () => {
+            setMobile(/Mobi|Android/i.test(navigator.userAgent));
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
   return (
     <Fragment>
       <LayoutOne
         headerContainerClass="container-fluid"
         headerPaddingClass="header-padding-2"
       >
-        <div className="BackgroundPicture pt-100 pb-100 d-flex justify-content-center">
-            <div className="col-lg-4  bg-white col-10"  >
-                <div className="d-flex col-12 mt-3 justify-content-around">
-                    <div className="col-lg-5 d-flex justify-content-center" style={{height:"40px" ,fontFamily: "Ethnocentric",borderBottom:"1px solid black"}}>
-                            Personal Details
+        <div className="BackgroundPicture pt-100 pb-100 d-flex justify-content-center" >
+            <div className="col-lg-7  bg-white col-10  pt-3" style={{borderRadius:"10px"}} >
+            <h1 className="text-center signup-heading">REGISTRATION</h1>
+                <div className="d-flex col-12 mt-3 list-container" >
+                    {!Mobile && <div className="d-flex justify-content-start inner-section">
+                            <div className="d-flex justify-content-center pt-2 number" >
+                                1
+                            </div>
+                            <div className={`d-flex justify-content-start p-2 ${currentPart==1?"heading-active":"heading"}`} >
+                            Personal 
+                            </div>
+                    </div>}
+                    { !Mobile &&<div className="d-flex justify-content-start inner-section">
+                            <div className="d-flex justify-content-center pt-2 number" >
+                                2
+                            </div>
+                            <div className={`d-flex justify-content-start p-2 ${currentPart==2?"heading-active":"heading"}`} >
+                            Contact
+                            </div>
+                    </div>}
+
+                    {!Mobile &&<div className="d-flex justify-content-start inner-section">
+                            <div className="d-flex justify-content-center pt-2 number" >
+                                3
+                            </div>
+                            <div className={`d-flex justify-content-start p-2 ${currentPart==3?"heading-active":"heading"}`} >
+                            Submit
+                            </div>
+                    </div>}
+                    {Mobile && currentPart==1 && <div className="d-flex justify-content-start inner-section">
+                            <div className="d-flex justify-content-center pt-2 number" >
+                                1
+                            </div>
+                            <div className={`d-flex justify-content-start p-2 ${currentPart==1?"heading-active":"heading"}`} >
+                            Personal 
+                            </div>
+                    </div>}
+                    { Mobile && currentPart==2 &&<div className="d-flex justify-content-start inner-section">
+                            <div className="d-flex justify-content-center pt-2 number" >
+                                2
+                            </div>
+                            <div className={`d-flex justify-content-start p-2 ${currentPart==2?"heading-active":"heading"}`} >
+                            Contact
+                            </div>
+                    </div>}
+                    {Mobile && currentPart==3 &&<div className="d-flex justify-content-start inner-section">
+                            <div className="d-flex justify-content-center pt-2 number" >
+                                3
+                            </div>
+                            <div className={`d-flex justify-content-start p-2 ${currentPart==3?"heading-active":"heading"}`} >
+                            Submit
+                            </div>
+                    </div>}
+                  
+                </div>
+                {currentPart === 1 &&<div className={`col-12  mt-4 mb-5`} >
+                <div class="row g-3">
+                    <div className="col-lg-6 col-12 ">
+                        <h5>Name<span style={{color:"orange"}}>*</span></h5> 
+                        <input type="text" required />
                     </div>
-                    <div className="col-lg-5 d-flex justify-content-center" style={{height:"40px", fontFamily: "Ethnocentric",borderBottom:"1px solid black"}}>
-                           <p> Parents Details</p>
+                    <div className="col-lg-6 col-12 ">
+                        <h5>Father Name<span style={{color:"orange"}}>*</span></h5> 
+                        <input type="text" required />
                     </div>
                 </div>
-                {currentPart === 1 &&<div className={`col-12 bg-success mt-3`} style={{height:"300px"}}>
-                    div 1
-                    <button onClick={handleNext}>next</button>
+                <div class="row g-3 ">
+                    <div className="col-lg-6 col-12  ">
+                        <h5>CNIC/B-Form<span style={{color:"orange"}}>*</span></h5> 
+                        <input type="text" required />
+                    </div>
+                    <div className="col-lg-6 col-12 ">
+                        <h5>Weight (KG)<span style={{color:"orange"}}>*</span></h5> 
+                        <input type="number" required style={{marginTop:"5px",border:" 1px solid #CCCCCC"}} />
+                    </div>
+                </div>
+                <div class="row g-3">
+                    <div className="col-lg-6 col-12" style={{marginTop:"10px"}}>
+                        <h5 style={{paddingBottom:"3px"}}>Date of Birth<span style={{color:"orange"}}>*</span></h5> 
+                        <input type="date" required style={{border:" 1px solid #CCCCCC"}} />
+                    </div>
+                    <div className="col-lg-6 col-12 " style={{marginTop:"10px"}}>
+                        <h5 style={{paddingBottom:"3px"}}>Date of Joinning<span style={{color:"orange"}}>*</span></h5> 
+                        <input type="date" required style={{border:" 1px solid #CCCCCC"}}/>
+                    </div>
+                </div>
+                <div class="row g-3 " >
+                    <div className="col-lg-6 col-12 "style={{marginTop:"10px"}}>
+                        <h5>City<span style={{color:"orange"}}>*</span></h5> 
+                        <select className="selectform" >
+                            <option>Faisalabad</option>
+                            <option>Lahore</option>
+                        </select>
+                    </div>
+                    <div className="col-lg-6 col-12" style={{marginTop:"10px"}}>
+                        <h5>Branch<span style={{color:"orange"}}>*</span></h5> 
+                        <select className="selectform" >
+                            <option>Faisalabad</option>
+                            <option>Lahore</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row g-3" >
+                    <div className="col-lg-6 col-12 " style={{marginTop:"10px"}}>
+                        <h5>Gender<span style={{color:"orange"}}>*</span></h5> 
+                        <select className="selectform" >
+                            <option select >Male</option>
+                            <option>Female</option>
+                            <option>Other</option>
+                        </select>
+                      
+                    </div>
+                    <div className="col-lg-6 col-12" style={{marginTop:"10px"}}>
+                        <h5>Father<span style={{color:"orange"}}>*</span></h5> 
+                        <select className="selectform" >
+                            <option select>Alive</option>
+                            <option>Death</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row g-3" >
+                    <div className=" col-12" style={{marginTop:"20px"}}>
+                    <h5>Player Picture<span style={{color:"orange"}}>*</span></h5>
+                        <BlurredBackgroundModal></BlurredBackgroundModal>
+                    </div>
+                </div>
+             
+
+                    <div className="d-flex justify-content-end pt-3 mt-4">
+                        <button onClick={handleNext} className="next-btn">next</button>
+                    </div>
                 </div>}
-                {currentPart === 2 &&<div className={`col-12 bg-success mt-3`} style={{height:"300px"}}>
-                    div 2
-                    <button onClick={handlePrevious}>pre</button>
+                {currentPart === 2 &&<div className={`col-12 mt-3 mb-5`} >
+                <div class="row g-3 mt-3">
+                    <div className="col-lg-6 col-12 "style={{marginTop:"10px"}}>
+                        <h5>Email<span style={{color:"orange"}}>*</span></h5> 
+                        <input type="email" required />
+                    </div>
+                    <div className="col-lg-6 col-12 "style={{marginTop:"10px"}}>
+                        <h5>Player Contact Number<span style={{color:"orange"}}>*</span></h5> 
+                        <input type="tel" required />
+                    </div>
+                </div>
+                <div class="row g-3 mt-2">
+                    <div className="col-lg-6 col-12 " style={{marginTop:"10px"}}>
+                        <h5>Guardian Contact Number (if any)</h5> 
+                        <input type="email"  />
+                    </div>
+                    <div className="col-lg-6 col-12 " style={{marginTop:"10px"}}>
+                        <h5>Father Contact Number</h5> 
+                        <input type="tel" required />
+                    </div>
+                </div>
+                <div class="row g-3 mt-2">
+                    <div className="col-lg-6 col-12 " style={{marginTop:"10px"}}>
+                        <h5>username<span style={{color:"orange"}}>*</span></h5> 
+                        <input type="email"  readOnly />
+                    </div>
+                    <div className="col-lg-6 col-12 " style={{marginTop:"10px"}}>
+                        <h5>Password<span style={{color:"orange"}}>*</span></h5> 
+                        <input type={showPassword ? 'text' : 'password'} required/>
+                            
+                            <div className="d-flex justify-content-end" style={{paddingRight:"10px", marginTop:"-30px"}}> 
+                            {showPassword ? (<FontAwesomeIcon icon={faEyeSlash} onClick={togglePasswordVisibility} />) : (<FontAwesomeIcon icon={faEye} onClick={togglePasswordVisibility}   />)}
+                            </div>
+                    </div>
+                </div>
+                <div class="row g-3 mt-3">
+                    <div className=" col-12 " style={{marginTop:"10px"}}>
+                        <h5>Home Address<span style={{color:"orange"}}>*</span></h5> 
+                        <textarea className="textarea" style={{backgroundColor:"#ECEFF8"}} ></textarea>
+                    </div>
+                </div>
+                    <div className="d-flex justify-content-end pt-3 mt-4"style={{columnGap:"7px"}}>
+                        <button onClick={handlePrevious}className="pre-btn">Back</button>
+                        <button onClick={handleNext} className="next-btn">Next</button>
+                    </div>
+                </div>}
+                {currentPart === 3 &&<div className={`col-12 mt-3`} style={{height:"300px"}}>
+                    div 3
+                    <div className="d-flex justify-content-end pt-3" style={{columnGap:"7px"}}>
+                    <button onClick={handlePrevious}className="pre-btn" >BACK</button>
+                        <button  className="next-btn">Submit</button>
+                    </div>
                 </div>}
             </div>
         </div>
@@ -197,6 +390,7 @@ const SignUp = (props) => {
 };
 
 export default SignUp;
+
 
 
 
